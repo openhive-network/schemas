bidonjour/hive

Add Array schema

tests/schemas/package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Int, Map, Null, String
+from schemas.__private.fundamental_schemas import Any_ as Any, Array, Bool, Float, Int, Map, Null, String

openhive-network/schemas

package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Int, Map, Null, Str
+from schemas.__private.fundamental_schemas import Any_ as Any, Array, Bool, Float, Int, Map, Null, Str