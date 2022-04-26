bidonjour/hive

Add following fundamental schemas: Any, Bool, Float, Null

tests/schemas/package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import String
+from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Null, String

openhive-network/schemas

package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Str
+from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Null, Str