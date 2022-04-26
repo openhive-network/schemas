openhive-network/hive

Add Date schema
Custom checker adds the ability to create your own validable types

tests/schemas/package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Any_ as Any, Array, ArrayStrict, Bool, Float, Int, Map, Null, String
+from schemas.__private.fundamental_schemas import Any_ as Any, Array, ArrayStrict, Bool, Date, Float, Int, Map, Null, String

openhive-network/schemas

package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Any_ as Any, Array, ArrayStrict, Bool, Float, Int, Map, Null, Str
+from schemas.__private.fundamental_schemas import Any_ as Any, Array, ArrayStrict, Bool, Date, Float, Int, Map, Null, Str