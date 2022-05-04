3b904fcb4b3b3fa7111c1d1b4fb2a997126f69a1
Michał Kudela <kudmich@syncad.com>
2022-05-04 13:06:31 +0200

    Add database_api initialization files

---
+++ package/schemas/database_api/__init__.py
@@ -0,0 +1,3 @@
+from schemas.database_api.response_schemas import (
+    get_dynamic_global_properties,
+)
---
+++ package/schemas/database_api/response_schemas.py
@@ -0,0 +1,5 @@
+from schemas.predefined import *
+
+# FIXME, Workaround: Method necessary to prepare the test environment for 'wallet bridge API' tests, used by TestTools.
+#  Add a schema when developing the tests database API.
+get_dynamic_global_properties = Any()
