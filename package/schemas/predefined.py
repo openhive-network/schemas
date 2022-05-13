2113a928719f1b52fe5fd301ada076f1133b6a40
Michał Kudela <kudmich@syncad.com>
2022-05-13 09:43:32 +0200

    Refactor 'predefined' file

--- package/schemas/predefined.py
+++ package/schemas/predefined.py
@@ -1,2 +1,28 @@
-from schemas.__private.fundamental_schemas import AllOf, Any_ as Any, AnyOf, Array, ArrayStrict, Bool, Date, Float, Int, Map, Null, OneOf, Str
-from schemas.__private.custom_schemas import AssetHbd, AssetHive, AssetVests, Authority, TransactionId, PublicKey, Manabar, Price, Proposal, HardforkVersion
+from schemas.__private.fundamental_schemas import (
+    AllOf,
+    AnyOf,
+    Any_ as Any,
+    Array,
+    ArrayStrict,
+    Bool,
+    Date,
+    Float,
+    Int,
+    Map,
+    Null,
+    OneOf,
+    Str,
+)
+
+from schemas.__private.custom_schemas import (
+    AssetHbd,
+    AssetHive,
+    AssetVests,
+    Authority,
+    HardforkVersion,
+    Manabar,
+    Price,
+    Proposal,
+    PublicKey,
+    TransactionId,
+)
