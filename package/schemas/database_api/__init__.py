352337d46561965220266c8bb081f00136f6e405
Mateusz Kudela <mkudela@syncad.com>
2022-05-19 10:40:44 +0200

    Add schemas for database_api methods

--- package/schemas/database_api/__init__.py
+++ package/schemas/database_api/__init__.py
@@ -1,3 +1,12 @@
 from schemas.database_api.response_schemas import (
-    get_dynamic_global_properties,
+    get_dynamic_global_properties, find_account_recovery_requests, find_accounts, find_change_recovery_account_requests,
+    find_collateralized_conversion_requests, find_comments, find_comments, find_decline_voting_rights_requests,
+    find_escrows, find_hbd_conversion_requests, find_limit_orders, find_owner_histories, find_proposals,
+    find_recurrent_transfers, find_savings_withdrawals, find_vesting_delegation_expirations, find_vesting_delegations,
+    find_withdraw_vesting_routes, find_witnesses, get_comment_pending_payouts, get_order_book, get_potential_signatures,
+    get_required_signatures, get_transaction_hex, is_known_transaction, list_account_recovery_requests, list_accounts,
+    list_change_recovery_account_requests, list_collateralized_conversion_requests, list_decline_voting_rights_requests,
+    list_escrows, list_hbd_conversion_requests, list_limit_orders, list_owner_histories, list_savings_withdrawals,
+    list_vesting_delegation_expirations, list_vesting_delegations, list_withdraw_vesting_routes, list_witness_votes,
+    list_witnesses, verify_authority, list_proposal_votes, verify_signatures, list_proposals,
 )
