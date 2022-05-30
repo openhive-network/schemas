80e11be00fe61ca77ca4fcff713045d64ccddc1d
Michał Kudela <kudmich@syncad.com>
2022-05-30 11:36:12 +0200

    get_schema finishes using __init__s from wallet_bridge and database APIs

--- package/schemas/database_api/__init__.py
+++
@@ -1,3 +0,0 @@
-from schemas.database_api.response_schemas import (
-    get_dynamic_global_properties,
-)
--- package/schemas/get_schema.py
+++ package/schemas/get_schema.py
@@ -1,11 +1,18 @@
+from __future__ import annotations
+
 import importlib
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from schemas.__private.fundamental_schemas import Schema
 
 
-def get_schema(schema_name):
+def get_schema(schema_name: str) -> Schema:
     api, method = schema_name.split('.')
-    schemas_module = importlib.import_module(f'schemas.{api}')
+    schemas_module = importlib.import_module(f'schemas.{api}.response_schemas')
     try:
         return getattr(schemas_module, method)
     except:
-        raise AttributeError(f'The \'{method}\' method in \'{api}\' api does not have a matching schema.'
-                             f' This means that the schema does not exist.')
+        raise AttributeError(f'The "{method}" method in "{api}" API does not have a matching schema.'
+                             f' This means that the schema does not exist.'
+                             f' Add "{method}" schema definition to {schemas_module.__file__}')
