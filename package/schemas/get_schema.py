9488e22a6f82f1b24c9cd081f8eadde0c26bbfaa
Michał Kudela <kudmich@syncad.com>
2022-06-01 14:07:05 +0200

    test

--- package/schemas/get_schema.py
+++ package/schemas/get_schema.py
@@ -3,13 +3,23 @@ from __future__ import annotations
 import importlib
 from typing import TYPE_CHECKING
 
+from test_tools import logger
+
+
 if TYPE_CHECKING:
     from schemas.__private.fundamental_schemas import Schema
 
 
 def get_schema(schema_name: str) -> Schema:
     api, method = schema_name.split('.')
-    schemas_module = importlib.import_module(f'schemas.{api}.response_schemas')
+
+    try:
+        schemas_module = importlib.import_module(f'schemas.{api}.response_schemas')
+        logger.info(f'Successfully imported module: "schemas.{api}.response_schemas"')
+    except ModuleNotFoundError:
+        logger.info(f'Failed to import module: "schemas.{api}.response_schemas"')
+        raise
+
     try:
         return getattr(schemas_module, method)
     except:
