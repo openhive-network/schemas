ea584cbdb234a285a9d73b34ccf32ffe9447a99f
Michał Kudela <kudmich@syncad.com>
2022-08-19 14:59:36 +0200

    Add schemas to `jsonrpc` API

--- package/schemas/jsonrpc/response_schemas.py
+++ package/schemas/jsonrpc/response_schemas.py
@@ -3,6 +3,23 @@ from schemas.predefined import *
 get_methods = Array(Str())
 
 get_signature = Map({
+<<<<<<< HEAD
     'args': Any(),
     'ret': Any(),
+=======
+    'args': AnyOf(
+        Array(),
+        Map({}, allow_additional_properties=True),
+        Null(),
+    ),
+    'ret': AnyOf(
+        Array(),
+        Bool(),
+        HardforkVersion(),
+        Int(),
+        Map({}, allow_additional_properties=True),
+        Null(),
+        Str(pattern=''),
+    ),
+>>>>>>> 2a73928 (Add schemas to `jsonrpc` API)
 })
