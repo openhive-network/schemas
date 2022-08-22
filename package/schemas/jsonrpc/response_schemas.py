7141a3547aff378764e9a5dd055c153ad1fd7aa8
Michał Kudela <kudmich@syncad.com>
2022-08-22 08:50:57 +0200

    STASH

--- package/schemas/jsonrpc/response_schemas.py
+++ package/schemas/jsonrpc/response_schemas.py
@@ -2,10 +2,39 @@ from schemas.predefined import *
 
 get_methods = Array(Str())
 
-get_signature = Map({
-    'args': AnyOf(
-        Array(),
-        Map({}, allow_additional_properties=True),
-    ),
-    'ret': Map({}, allow_additional_properties=True)
-})
+def select_this_one(request) -> bool:
+    return isinstance(request.params['1'], int)
+
+#TODO schema switch , conditional schema
+get_signature = SuperWrapper([
+    [
+        select_this_one,
+        Map({
+            'args': AnyOf(
+                Array(),
+                Map({}, allow_additional_properties=True),
+            ),
+            'ret': Map({}, allow_additional_properties=True)
+        },
+    ],
+    [
+        select_this_one,
+        Map({
+            'args': AnyOf(
+                Array(),
+                Map({}, allow_additional_properties=True),
+            ),
+            'ret': Map({}, allow_additional_properties=True)
+        },
+    ],
+    [
+        select_this_one,
+        Map({
+            'args': AnyOf(
+                Array(),
+                Map({}, allow_additional_properties=True),
+            ),
+            'ret': Map({}, allow_additional_properties=True)
+        },
+    ],
+)
