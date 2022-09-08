a4f254c9cab8690c0937dbd74b07a9990a499072
Michał Kudela <kudmich@syncad.com>
2022-09-08 09:50:06 +0200

    Create a `Request` class

--- package/schemas/get_schema.py
+++ package/schemas/get_schema.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import importlib
-from typing import TYPE_CHECKING
+from typing import Any, Dict, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from schemas.__private.fundamental_schemas import Schema
+
+from schemas.__private.fundamental_schemas import Schema
 
 
 def get_schema(schema_name: str) -> Schema:
@@ -16,3 +18,11 @@ def get_schema(schema_name: str) -> Schema:
         raise AttributeError(f'The "{method}" method in "{api}" API does not have a matching schema.'
                              f' This means that the schema does not exist.'
                              f' Add "{method}" schema definition to {schemas_module.__file__}')
+
+
+class Request:
+    def __init__(self, message: Dict[str, Any]):
+        api_name, method_name = message['method'].split('.')
+        self.api_name: str = api_name
+        self.method_name: str = method_name
+        self.params = message['params']
