0d1c84c6dca0d8a50d68b37a866f4fd273e6447d
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-13 14:58:58 +0200

    Change memo to str type, create base abstract class for all assets

--- package/schemas/operations/transfer_operation.py
+++ package/schemas/operations/transfer_operation.py
@@ -7,11 +7,11 @@ from pydantic import Field
 from schemas.operations.preconfigure_base_model import PreconfiguredBaseModel
 
 if TYPE_CHECKING:
-    from schemas.__private.hive_fields_schemas import AccountName, LegacyAssetHbd, LegacyAssetHive, PublicKey
+    from schemas.__private.hive_fields_schemas import AccountName, LegacyAssetHbd, LegacyAssetHive
 
 
 class TransferOperation(PreconfiguredBaseModel):
     from_: AccountName = Field(..., alias="from")
     to: AccountName
     amount: LegacyAssetHive | LegacyAssetHbd
-    memo: PublicKey
+    memo: str
