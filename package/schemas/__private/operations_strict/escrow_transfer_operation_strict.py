f6896026aece03c9c3ad9cedb1fbce4ebf529658
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-18 15:10:03 +0200

    Create separate types module for type aliases

--- package/schemas/__private/operations_strict/escrow_transfer_operation_strict.py
+++ package/schemas/__private/operations_strict/escrow_transfer_operation_strict.py
@@ -9,13 +9,12 @@ from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 if TYPE_CHECKING:
     from schemas.__private.hive_fields_schemas import (
         AccountName,
-        AssetHbd,
         AssetHbdLegacy,
-        AssetHive,
         AssetHiveLegacy,
         HiveDateTime,
         Uint32t,
     )
+    from schemas.types import AssetHbd, AssetHive
 
 
 class EscrowTransferOperationStrict(PreconfiguredBaseModel):
--- package/schemas/operations/update_proposal_operation.py
+++ package/schemas/operations/update_proposal_operation.py
@@ -5,7 +5,8 @@ from typing import TYPE_CHECKING
 from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 if TYPE_CHECKING:
-    from schemas.__private.hive_fields_schemas import AccountName, AssetHbd, AssetHbdLegacy, HiveDateTime, Int64t
+    from schemas.__private.hive_fields_schemas import AccountName, AssetHbdLegacy, HiveDateTime, Int64t
+    from schemas.types import AssetHbd
 
 
 class UpdateProposalOperation(PreconfiguredBaseModel):
