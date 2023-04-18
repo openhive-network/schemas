c9b6207ebd990bc5f4894e1d3f30524084dd81ba
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-18 09:06:46 +0200

    Fix typo in preconfigured_base_model, move this file to __private/

--- package/schemas/operations/custom_json_operation.py
+++ package/schemas/operations/custom_json_operation.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Any
 
 from pydantic import Field, Json
 
-from schemas.operations.preconfigure_base_model import PreconfiguredBaseModel
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 if TYPE_CHECKING:
     from schemas.__private.hive_fields_schemas import AccountName, CustomIdType
--- package/schemas/operations/set_reset_account_operation.py
+++ package/schemas/operations/set_reset_account_operation.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from schemas.operations.preconfigure_base_model import PreconfiguredBaseModel
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 if TYPE_CHECKING:
     from schemas.__private.hive_fields_schemas import AccountName
