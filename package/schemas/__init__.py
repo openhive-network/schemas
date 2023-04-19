553508ad320ca7582fb2ac1a830e686b399661e9
Mateusz Żebrak <mzebrak@syncad.com>
2023-04-19 13:28:24 +0200

    Change modules structure to expose api for importing

---
+++ package/schemas/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from schemas import operations, operations_strict
+
+__all__ = [
+    "operations",
+    "operations_strict",
+]
---
+++ package/schemas/__private/operations_strict/__init__.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from schemas.__private.operations_strict.account_update2_operation_strict import AccountUpdate2OperationStrict
+from schemas.__private.operations_strict.account_update_operation_strict import AccountUpdateOperationStrict
+from schemas.__private.operations_strict.account_witness_vote_operation_strict import AccountWitnessVoteOperationStrict
+from schemas.__private.operations_strict.cancel_transfer_from_savings_operation_strict import (
+    CancelTransferFromSavingsOperationStrict,
+)
+from schemas.__private.operations_strict.collateralized_convert_operation_strict import (
+    CollateralizedConvertOperationStrict,
+)
+from schemas.__private.operations_strict.comment_options_operation_strict import CommentOptionsOperationStrict
+from schemas.__private.operations_strict.convert_operation_strict import ConvertOperationStrict
+from schemas.__private.operations_strict.decline_voting_rights_operation_strict import (
+    DeclineVotingRightsOperationStrict,
+)
+from schemas.__private.operations_strict.escrow_approve_operation_strict import EscrowApproveOperationStrict
+from schemas.__private.operations_strict.escrow_dispute_operation_strict import EscrowDisputeOperationStrict
+from schemas.__private.operations_strict.escrow_release_operation_strict import EscrowReleaseOperationStrict
+from schemas.__private.operations_strict.escrow_transfer_operation_strict import EscrowTransferOperationStrict
+from schemas.__private.operations_strict.limit_order_cancel_operation_strict import LimitOrderCancelOperationStrict
+from schemas.__private.operations_strict.limit_order_create2_operation_strict import LimitOrderCreate2OperationStrict
+from schemas.__private.operations_strict.limit_order_create_operation_strict import LimitOrderCreateOperationStrict
+from schemas.__private.operations_strict.recurrent_transfer_operation_strict import RecurrentTransferOperationStrict
+from schemas.__private.operations_strict.set_withdraw_vesting_route_operation_strict import (
+    SetWithdrawVestingRouteOperationStrict,
+)
+from schemas.__private.operations_strict.transfer_from_savings_operation_strict import (
+    TransferFromSavingsOperationStrict,
+)
+from schemas.__private.operations_strict.transfer_to_vesting_operation_strict import TransferToVestingOperationStrict
+from schemas.__private.operations_strict.update_proposal_votes_operation_strict import (
+    UpdateProposalVotesOperationStrict,
+)
+from schemas.__private.operations_strict.vote_operation_strict import VoteOperationStrict
+
+__all__ = [
+    "AccountUpdate2OperationStrict",
+    "AccountUpdateOperationStrict",
+    "AccountWitnessVoteOperationStrict",
+    "CancelTransferFromSavingsOperationStrict",
+    "CollateralizedConvertOperationStrict",
+    "CommentOptionsOperationStrict",
+    "ConvertOperationStrict",
+    "DeclineVotingRightsOperationStrict",
+    "EscrowApproveOperationStrict",
+    "EscrowDisputeOperationStrict",
+    "EscrowReleaseOperationStrict",
+    "EscrowTransferOperationStrict",
+    "LimitOrderCancelOperationStrict",
+    "LimitOrderCreate2OperationStrict",
+    "LimitOrderCreateOperationStrict",
+    "RecurrentTransferOperationStrict",
+    "SetWithdrawVestingRouteOperationStrict",
+    "TransferFromSavingsOperationStrict",
+    "TransferToVestingOperationStrict",
+    "UpdateProposalVotesOperationStrict",
+    "VoteOperationStrict",
+]
