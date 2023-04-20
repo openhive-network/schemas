166578e35aa8b1a8f5455764d4793c09acac7578
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-20 10:06:55 +0200

    Fix TypeError in CustomJsonOperation

--- schemas/__private/operations/custom_json_operation.py
+++ schemas/__private/operations/custom_json_operation.py
@@ -12,4 +12,4 @@ class CustomJsonOperation(PreconfiguredBaseModel):
     required_auths: list[AccountName]
     required_posting_auths: list[AccountName]
     id_: CustomIdType = Field(..., alias="id")
-    json_: Json[Any] = Field(..., alias="json") | EmptyString
+    json_: Json[Any] | EmptyString = Field(..., alias="json")
