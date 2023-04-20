0cb3abdd3fb0b638e19f22b1cb180326d0c65075
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-20 09:22:22 +0200

    Add docstrings in hive_fields_schemas files

--- schemas/__private/hive_fields_schemas.py
+++ schemas/__private/hive_fields_schemas.py
@@ -1,3 +1,10 @@
+"""
+    It is module with hive fields to use in python code. It helps to create hive fields without remember about
+    for example fill nai when creating AssetNai. In situation when you can't find here field that you need that's
+    probably mean that there was not need for creating strict and non-strict version of field and you will find it
+    in hive_fields_schemas_strict without 'strict' suffix
+"""
+
 from __future__ import annotations
 
 from datetime import datetime
