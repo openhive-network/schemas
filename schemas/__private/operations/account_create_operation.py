27a26f3d4ca04a66bba7e1cf7c90b6b63e077df1
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-20 14:36:36 +0200

    Modify test of HiveInt

--- schemas/__private/operations/account_create_operation.py
+++ schemas/__private/operations/account_create_operation.py
@@ -6,16 +6,16 @@ from pydantic import Json
 
 from schemas.__private.hive_fields_schemas import (
     AccountName,
-    AssetHive,
     Authority,
     EmptyString,
     PublicKey,
 )
+from schemas.__private.hive_fields_schemas_strict import AssetHiveStrict
 from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 
 class AccountCreateOperation(PreconfiguredBaseModel):
-    fee: AssetHive
+    fee: AssetHiveStrict
     creator: AccountName
     new_account_name: AccountName
     owner: Authority
