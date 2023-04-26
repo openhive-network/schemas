e563d136ba0c4dfc13249acc7d748c89708928a2
Mateusz Żebrak <mzebrak@syncad.com>
2023-04-26 09:57:13 +0200

    Implement as_strict_model

--- schemas/__private/preconfigured_base_model.py
+++ schemas/__private/preconfigured_base_model.py
@@ -5,10 +5,33 @@
 
 from __future__ import annotations
 
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, create_model
+from typing_extensions import Self
 
 
 class PreconfiguredBaseModel(BaseModel):
     class Config:
         extra = Extra.forbid
         allow_population_by_field_name = True
+
+    @classmethod
+    def as_strict_model(cls, recursively: bool = True) -> type[Self]:
+        """
+        Generate a BaseModel class with all the same fields like the class on which the method was called but with
+        required fields only (no defaults allowed).
+
+        Returns:
+            Strict version of the class
+        """
+
+        # ellipsis is used to indicate that the field is required
+        field_definitions = {field.name: (field.type_, ...) for field in cls.__fields__.values()}
+
+        if recursively:
+            for field_name, pack in field_definitions.items():
+                field_type = pack[0]
+                if issubclass(field_type, PreconfiguredBaseModel):
+                    strict_type = field_type.as_strict_model(recursively=True)
+                    field_definitions[field_name] = (strict_type, ...)
+
+        return create_model(f"{cls.__name__}Strict", **field_definitions)  # type: ignore
