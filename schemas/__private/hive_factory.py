5a618a7bfd402a2946274e371b43b167ff64791e
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-04 14:30:06 +0200

    Add HiveResult class with factory method

---
+++ schemas/__private/hive_factory.py
@@ -0,0 +1,25 @@
+"""
+Response from API has three poles -> id, jsonrpc and result. The factory method from HiveResult class is using to
+return result pole, cause is must be validated deeper and then all operations is being performed on result field.
+Yoy must use it like this -> HiveResult.factory(type_of_response, **response_from_api_json/dict).
+"""
+from __future__ import annotations
+
+from typing import Any, Generic, TypeVar
+
+from pydantic import Field
+from pydantic.generics import GenericModel
+
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+
+T = TypeVar("T", bound=PreconfiguredBaseModel)
+
+
+class HiveResult(GenericModel, Generic[T]):
+    id_: int = Field(..., alias="id")
+    jsonrpc: str
+    result: T
+
+    @staticmethod
+    def factory(t: Any, **kwargs: Any) -> HiveResult[T]:
+        return HiveResult[t](**kwargs)
