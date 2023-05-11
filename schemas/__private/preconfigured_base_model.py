24eefcd476da1041a563a6ecb2b64517356bb54e
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-11 13:29:57 +0200

    Add json method to PreconfiguredBaseModel

--- schemas/__private/preconfigured_base_model.py
+++ schemas/__private/preconfigured_base_model.py
@@ -5,14 +5,51 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Any
+
 from pydantic import BaseModel, Extra, create_model
 from typing_extensions import Self
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Mapping, Set
 
 
 class PreconfiguredBaseModel(BaseModel):
     class Config:
         extra = Extra.forbid
         allow_population_by_field_name = True
+
+    def json(
+        self,
+        *,
+        include: Set[int | str] | Mapping[int | str, Any] | None = None,
+        exclude: Set[int | str] | Mapping[int | str, Any] | None = None,
+        by_alias: bool = False,
+        skip_defaults: bool | None = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+        encoder: Callable[[Any], Any] | None = None,
+        models_as_dict: bool = True,
+        **dumps_kwargs: Any,
+    ) -> str:
+        value = {
+            "type": self.__repr_name__().lower(),
+            "value": super().json(
+                include=include,
+                exclude=exclude,
+                by_alias=by_alias,
+                skip_defaults=skip_defaults,
+                exclude_unset=exclude_unset,
+                exclude_defaults=exclude_defaults,
+                exclude_none=exclude_none,
+                encoder=encoder,
+                models_as_dict=models_as_dict,
+                **dumps_kwargs,
+            ),
+        }
+
+        return str(value)
 
     @classmethod
     def as_strict_model(cls, recursively: bool = True) -> type[Self]:
