63d3d92d6f86d9c675b698933071d637ccfa020e
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-11 09:44:11 +0200

    Add tests of network_broadcast_api

---
+++ tests/unit/test_network_broadcast_api.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import pytest
+from pydantic import ValidationError
+
+from schemas.network_broadcast_api.response_schemas import BroadcastTransaction
+
+
+def test_correct_values_broadcast_transaction() -> None:
+    # ACT
+    test_instance = BroadcastTransaction()
+
+    # ASSERT
+    assert bool(test_instance.dict()) is False
+
+
+@pytest.mark.parametrize("parameter", ({"a": "b"}, {"a": 1, "b": 2, "c": 3}))
+def test_incorrect_values_broadcast_transaction(parameter: dict[str, int | str]) -> None:
+    # ACT & ASSERT
+    with pytest.raises(ValidationError):
+        BroadcastTransaction(**parameter)
