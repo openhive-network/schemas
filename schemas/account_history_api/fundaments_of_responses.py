681a9bced086a4908582041c3fe687536cb58563
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-16 12:36:32 +0200

    Add all schemas of responses from account_history_api

--- schemas/account_history_api/fundaments_of_responses.py
+++ schemas/account_history_api/fundaments_of_responses.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from schemas.__private.hive_fields_basic_schemas import HiveInt
+from schemas.__private.hive_fields_basic_schemas import HiveDateTime, HiveInt
+from schemas.__private.hive_fields_custom_schemas import ApiOperationObject
 from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 
-class EnumVirtualOpsFundament(PreconfiguredBaseModel):
-    ops: list[int]
-    ops_by_block: list[int]
-    next_block_range_begin: HiveInt
-    next_operation_begin: HiveInt
+class EnumVirtualOpsFieldFundament(PreconfiguredBaseModel):
+    """This is a field for ops_by_block field in EnumVirtualOps model"""
+
+    block: HiveInt
+    timestamp: HiveDateTime
+    irreversible: bool
+    ops: list[ApiOperationObject]
--- schemas/account_history_api/response_schemas.py
+++ schemas/account_history_api/response_schemas.py
@@ -1,7 +1,35 @@
 from __future__ import annotations
 
+from typing import Any
+
+from schemas.__private.hive_fields_basic_schemas import HiveDateTime, HiveInt
+from schemas.__private.hive_fields_custom_schemas import ApiOperationObject, Signature, TransactionId
+from schemas.__private.operations import OperationType
 from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+from schemas.account_history_api.fundaments_of_responses import EnumVirtualOpsFieldFundament
 
 
 class EnumVirtualOps(PreconfiguredBaseModel):
-    pass
+    ops: list[ApiOperationObject]
+    ops_by_block: list[EnumVirtualOpsFieldFundament]
+    next_block_range_begin: HiveInt
+    next_operation_begin: HiveInt
+
+
+class GetAccountHistory(PreconfiguredBaseModel):
+    history: list[tuple[HiveInt, ApiOperationObject]]
+
+
+class GetOpsInBlock(PreconfiguredBaseModel):
+    ops: list[ApiOperationObject]
+
+
+class GetTransaction(PreconfiguredBaseModel):
+    block_num: HiveInt
+    expiration: HiveDateTime
+    extensions: list[Any]
+    operations: list[dict[str, OperationType]]
+    ref_block_num: HiveInt
+    ref_block_prefix: HiveInt
+    signatures: list[Signature]
+    transaction_id: TransactionId
