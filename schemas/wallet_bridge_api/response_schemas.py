483305171588c4860a691499b7f80a4ff073d5e8
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-19 15:16:47 +0200

    Add wallet_bridge_api schemas

---
+++ schemas/wallet_bridge_api/response_schemas.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import Generic
+
+from pydantic import Field
+from pydantic.generics import GenericModel
+
+from schemas.__private.hive_fields_basic_schemas import AssetHive, AssetVests, HiveInt
+from schemas.__private.hive_fields_custom_schemas import RcAccountObject, TransactionId
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+
+
+class BroadcastTransactionSynchronous(PreconfiguredBaseModel):
+    block_num: HiveInt
+    expired: bool
+    id_: TransactionId = Field(..., alias="id")
+    rc_cost: HiveInt | None
+    trx_num: HiveInt
+
+
+FindRcAccounts = list[RcAccountObject[AssetVests]]
+
+
+class GetChainProperties(PreconfiguredBaseModel, GenericModel, Generic[AssetHive]):
+    account_creation_fee: AssetHive
+    maximum_block_size: HiveInt
+    hbd_interest_rate: HiveInt
+    account_subsidy_budget: HiveInt
+    account_subsidy_decay: HiveInt
