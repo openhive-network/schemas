c577b30eed4cb4e4e6f9b745d2432a1a488478f7
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-23 16:37:27 +0200

    Add another schemas of condenser_api responses

--- schemas/condenser_api/fundaments_of_responses.py
+++ schemas/condenser_api/fundaments_of_responses.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Any
+
 from pydantic import Field
 
 import schemas.block_api.fundaments_of_responses as fundaments_block_api
@@ -9,11 +11,18 @@ from schemas.__private.hive_fields_basic_schemas import (
     AssetHbdLegacy,
     AssetHiveLegacy,
     AssetVestsLegacy,
+    EmptyString,
     HiveDateTime,
     HiveInt,
 )
+from schemas.__private.hive_fields_custom_schemas import Permlink
 from schemas.__private.operation_objects import LegacyApiAllOperationObject
 from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+
+
+class GetAccountReputationsFundament(PreconfiguredBaseModel):
+    account: AccountName
+    reputation: HiveInt
 
 
 class GetAccountsFundament(
@@ -21,12 +30,13 @@ class GetAccountsFundament(
 ):
     """Base for this response is list_accounts from database api. Some additional fields are here and two excluded"""
 
-    last_post_edit = Field(None, exclude=True)  # type: ignore
-    is_smt = Field(None, exclude=True)  # type: ignore
+    last_post_edit: HiveDateTime | None = Field(None, exclude=True)  # type: ignore
+    is_smt: bool | None = Field(None, exclude=True)  # type: ignore
 
     post_history: list[str]
     vote_history: list[str]
     witness_votes: list[str]
+    vesting_balance: AssetHiveLegacy
     transfer_history: list[str]
     voting_power: HiveInt
     market_history: list[str]
@@ -52,9 +62,7 @@ class GetActiveVotesFundament(PreconfiguredBaseModel):
 class GetBlockFundament(fundaments_block_api.Block):
     """Identical as in block_api, just extensions changed"""
 
-
-class GetBlockHeaderFundament(fundaments_block_api.GetBlockHeaderFundament):
-    """Identical as in block_api, just extensions changed"""
+    extensions: list[tuple[str, Any]]
 
 
 class GetBlogFundament(fundaments_database_api.FindCommentsFundament[AssetHbdLegacy]):
@@ -72,3 +80,39 @@ class GetBlogEntriesFundament(PreconfiguredBaseModel):
     blog: AccountName
     reblog_on: HiveDateTime
     entry_id: HiveInt
+
+
+class ActiveVotes(PreconfiguredBaseModel):
+    """This is field which is used in GetCommentDiscussionsByPayout model"""
+
+    percent: HiveInt
+    reputation: HiveInt
+    rshares: HiveInt
+    voter: AccountName
+
+
+class GetCommentDiscussionsByPayoutFundament(fundaments_database_api.FindCommentsFundament[AssetHbdLegacy]):
+    id_: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    abs_rshares: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    vote_rshares: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    children_abs_rshares: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    max_cashout_time: HiveDateTime | None = Field(None, exclude=True)  # type: ignore
+    total_vote_weight: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    reward_weight: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    author_rewards: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    net_votes: HiveInt | None = Field(None, exclude=True)  # type: ignore
+    root_author: AccountName | EmptyString | None = Field(None, exclude=True)  # type: ignore
+    root_permlink: Permlink | EmptyString | None = Field(None, exclude=True)  # type: ignore
+    allow_replies: bool | None = Field(None, exclude=True)  # type: ignore
+    allow_votes: bool | None = Field(None, exclude=True)  # type: ignore
+    allow_curation_rewards: bool | None = Field(None, exclude=True)  # type: ignore
+
+    pending_payout_value: AssetHbdLegacy
+    promoted: AssetHbdLegacy
+    replies: list[str]
+    body_length: HiveInt
+    author_reputation: HiveInt
+    url: str
+    root_title: str
+    post_id: HiveInt
+    active_votes: list[ActiveVotes]
--- schemas/condenser_api/response_schemas.py
+++ schemas/condenser_api/response_schemas.py
@@ -1,29 +1,34 @@
 from __future__ import annotations
 
+from typing import Any, Literal
+
 from pydantic import Field
 
 import schemas.block_api.fundaments_of_responses as fundaments_block_api
 import schemas.database_api.fundaments_of_reponses as fundaments_database_api
 import schemas.database_api.response_schemas as database_api
 import schemas.network_broadcast_api.response_schemas as broadcast_api
+import schemas.wallet_bridge_api.fundaments_of_responses as fundaments_wallet_bridge_api
 import schemas.wallet_bridge_api.response_schemas as wallet_bridge_api
 from schemas.__private.hive_fields_basic_schemas import (
     AccountName,
     AssetHbdLegacy,
     AssetHiveLegacy,
     AssetVestsLegacy,
+    EmptyString,
+    HiveDateTime,
     HiveInt,
 )
-from schemas.__private.hive_fields_custom_schemas import Proposal
-from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+from schemas.__private.hive_fields_custom_schemas import Price, Proposal
 from schemas.condenser_api.fundaments_of_responses import (
     GetAccountHistoryFundament,
+    GetAccountReputationsFundament,
     GetAccountsFundament,
     GetActiveVotesFundament,
     GetBlockFundament,
-    GetBlockHeaderFundament,
     GetBlogEntriesFundament,
     GetBlogFundament,
+    GetCommentDiscussionsByPayoutFundament,
 )
 
 
@@ -38,7 +43,7 @@ class BroadcastTransactionSynchronous(wallet_bridge_api.BroadcastTransactionSync
 class FindProposals(Proposal[AssetHbdLegacy]):
     """This response is identical as Proposal field, but here Legacy format is required and status excluded"""
 
-    status: str = Field(None, exclude=True)  # type: ignore
+    status: str | None = Field(None, exclude=True)  # type: ignore
 
 
 FindRcAccounts = wallet_bridge_api.FindRcAccounts[AssetVestsLegacy]  # identical as in wallet_bridge_api, but legacy
@@ -51,12 +56,10 @@ class FindRecurrentTransfers(fundaments_database_api.FindRecurrentTransfersFunda
 GetAccountCount = HiveInt  # should return just integer
 
 
-GetAccountHistory = GetAccountHistoryFundament  # to change
+GetAccountHistory = list[HiveInt, GetAccountHistoryFundament]  # type: ignore
 
 
-class GetAccountReputations(PreconfiguredBaseModel):
-    account: AccountName
-    reputation: HiveInt
+GetAccountReputations = list[GetAccountReputationsFundament]
 
 
 GetAccounts = list[GetAccountsFundament]
@@ -66,13 +69,18 @@ GetActiveVotes = list[GetActiveVotesFundament]
 
 
 class GetActiveWitnesses(database_api.GetActiveWitnesses):
-    """Identical response as in database_api"""
+    """Identical response as in database_api, future_witnesses pole exclude"""
+
+    future_witnesses: list[AccountName | EmptyString] | None = Field(None, exclude=True)
 
 
 GetBlock = GetBlockFundament | fundaments_block_api.EmptyModel
 
 
-GetBlockHeader = list[GetBlockHeaderFundament]
+class GetBlockHeader(fundaments_block_api.GetBlockHeaderFundament):
+    """Identical as in block_api, just extensions changed"""
+
+    extensions: list[tuple[str, Any]]
 
 
 GetBlog = list[GetBlogFundament]
@@ -82,3 +90,48 @@ GetBlogEntries = list[GetBlogEntriesFundament]
 
 
 GetChainProperties = wallet_bridge_api.GetChainProperties[AssetHiveLegacy]  # like in wallet_bridge_api, just legacy
+
+
+GetCollateralizedConversionRequests = list[
+    fundaments_wallet_bridge_api.GetCollateralizedConversionRequestsFundament[AssetHiveLegacy, AssetHbdLegacy]
+]
+
+
+GetCommentDiscussionsByPayout = list[GetCommentDiscussionsByPayoutFundament]
+
+
+class GetConfig(database_api.GetConfig[AssetHiveLegacy, AssetHbdLegacy]):
+    """Field like in database_api, with few modifications"""
+
+    HBD_SYMBOL: Literal["HBD"]  # type: ignore
+    HIVE_SYMBOL: Literal["HIVE"]  # type: ignore
+    VESTS_SYMBOL: Literal["VESTS"]  # type: ignore
+    NEW_HIVE_TREASURY_ACCOUNT: AccountName
+
+
+class GetContent(fundaments_database_api.FindCommentsFundament[AssetHbdLegacy]):
+    """Base from database_api, some additional fields"""
+
+    active: HiveDateTime
+    url: str
+    root_title: str
+    pending_payout_value: AssetHiveLegacy
+    total_pending_payout_value: AssetHiveLegacy
+    active_votes: list[str]
+    replies: list[str]
+    author_reputation: HiveInt
+    promoted: AssetHiveLegacy
+    body_length: HiveInt
+    reblogged_by: list[str]
+
+
+class GetContentReplies(GetContent):
+    """Identical response like GetContent above"""
+
+
+"""Identical as in wallet_bridge_api, just Legacy Asset format"""
+GetConversionRequests = list[fundaments_wallet_bridge_api.GetConversionRequestsFundament[AssetHbdLegacy]]
+
+
+class GetCurrentMedianHistoryPrice(Price[AssetHiveLegacy, AssetHbdLegacy]):
+    """Identical response as Price field, Legacy format of Assets"""
