325f7a5b5cb122b8ed1f09d656d70adc668c3ef2
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-29 12:04:47 +0200

    Add schema of jsonrpc

---
+++ schemas/jsonrpc/response_schemas.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Any
+
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+
+"""List of methods available in api"""
+GetMethods = list[str]
+
+
+class GetSignature(PreconfiguredBaseModel):
+    args: Any
+    ret: Any
