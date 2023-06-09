b17996ccff2f870ec1fd58b69f3d831ff5b2d3cf
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-06-09 12:55:20 +0200

    Add test for transaction_status_api response schema

---
+++ tests/test_transaction_status_api/test_transaction_status_response_schemas.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from schemas.__private.hive_factory import HiveResult
+from schemas.transaction_status_api.response_schemas import FindTransaction
+from tests.test_transaction_status_api.response_schemas import FIND_TRANSACTION
+
+
+@pytest.mark.parametrize("schema, parameters", [(FindTransaction, FIND_TRANSACTION)])
+def test_transaction_status_api_correct_values(schema: Any, parameters: dict[str, Any]) -> None:
+    # ACT & ASSERT
+    HiveResult.factory(schema, **parameters)
