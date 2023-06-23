77a4be98c4f4d29247749b3db3bd4f02a22650ec
kmochocki <kmochocki@syncad.com>
2023-06-23 12:30:47 +0000

    Remove old schemas

--- .old/package/schemas/account_by_key_api/response_schemas.py
+++
@@ -1,7 +0,0 @@
-from schemas.predefined import *
-
-get_key_references = Map({
-    'accounts': Array(
-        Array(AccountName())
-    )
-})
--- .old/package/schemas/jsonrpc/response_schemas.py
+++
@@ -1,8 +0,0 @@
-from schemas.predefined import *
-
-get_methods = Array(Str())
-
-get_signature = Map({
-    'args': Any(),
-    'ret': Any(),
-})
