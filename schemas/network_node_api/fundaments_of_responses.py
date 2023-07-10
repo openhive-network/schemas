7524ad39d74efbb38d8a471f80c8035db9d0008d
kmochocki <kmochocki@syncad.com>
2023-07-10 11:57:10 +0000

    Add network_node_api

---
+++ schemas/network_node_api/fundaments_of_responses.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+
+from pydantic import ConstrainedStr
+
+from schemas.__private.hive_fields_basic_schemas import (
+    HiveInt,
+)
+from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
+
+
+class PublicKeyData(ConstrainedStr):
+    strict = True
+    min_length = 66
+    max_length = 66
+
+
+class ApiPeerStatus(PreconfiguredBaseModel):
+    version: HiveInt
+    host: str
+    info: Any
