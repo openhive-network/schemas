41c96f4055c00a973ef527b4a62427f03fdcbe9b
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-01 09:16:35 +0200

    Create Operation.get_name_with_suffix method

--- schemas/__private/preconfigured_base_model.py
+++ schemas/__private/preconfigured_base_model.py
@@ -123,8 +123,21 @@ class Operation(PreconfiguredBaseModel):
 
     @classmethod
     def get_name(cls) -> str:
-        """conversion name of operation from CamelCase to snake_case"""
-        return re.sub(r"(?<!^)(?=[A-Z])", "_", cls.get_class_name()).lower()
+        """
+        Get the name of the operation.
+
+        e.g. `transfer` for `TransferOperation`
+        """
+        return cls.get_name_with_suffix().replace("_operation", "")
+
+    @classmethod
+    def get_name_with_suffix(cls) -> str:
+        """
+        Get the name of the operation with the `_operation` suffix.
+
+        e.g. `transfer_operation` for `TransferOperation`
+        """
+        return re.sub(r"(?<!^)(?=[A-Z])", "_", cls.get_class_name()).lower()  # convert class name to snake case
 
 
 class VirtualOperation(Operation):
