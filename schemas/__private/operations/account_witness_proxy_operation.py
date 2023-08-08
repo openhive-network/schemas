61399b2c3c8d90a15e85eaea4bd57241028f8da4
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-08 10:07:17 +0200

    Proxy parameter can be empty-string if user want to delete proxy

--- schemas/__private/operations/account_witness_proxy_operation.py
+++ schemas/__private/operations/account_witness_proxy_operation.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from schemas.__private.hive_fields_basic_schemas import AccountName
+from schemas.__private.hive_fields_basic_schemas import AccountName, EmptyString
 from schemas.__private.preconfigured_base_model import Operation
 
 
 class AccountWitnessProxyOperation(Operation):
     account: AccountName
-    proxy: AccountName
+    proxy: AccountName | EmptyString
