ad164284364ddf6d8d9b6753a901109344b41a07
Mateusz Żebrak <mzebrak@syncad.com>
2023-09-18 11:43:21 +0200

    Get rid of dynamic API allocation so imports now can't be missing

--- schemas/get_schema.py
+++ schemas/get_schema.py
@@ -1,28 +1,44 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final
 
 if TYPE_CHECKING:
+    from types import ModuleType
+
     from schemas.__private.preconfigured_base_model import PreconfiguredBaseModel
 
 
-from schemas.apis.account_by_key_api import response_schemas as account_by_key_api  # noqa: F401
-from schemas.apis.account_history_api import response_schemas as account_history_api  # noqa: F401
-from schemas.apis.block_api import response_schemas as block_api  # noqa: F401
-from schemas.apis.condenser_api import response_schemas as condenser_api  # noqa: F401
-from schemas.apis.database_api import response_schemas as database_api  # noqa: F401
-from schemas.apis.debug_node_api import response_schemas as debug_node_api  # noqa: F401
-from schemas.apis.jsonrpc import response_schemas as jsonrpc  # noqa: F401
-from schemas.apis.market_history_api import response_schemas as market_history_api  # noqa: F401
-from schemas.apis.network_broadcast_api import response_schemas as network_broadcast_api  # noqa: F401
-from schemas.apis.network_node_api import response_schemas as network_node_api  # noqa: F401
-from schemas.apis.rc_api import response_schemas as rc_api  # noqa: F401
-from schemas.apis.reputation_api import response_schemas as reputation_api  # noqa: F401
-from schemas.apis.transaction_status_api import response_schemas as transaction_status_api  # noqa: F401
-from schemas.apis.wallet_bridge_api import response_schemas as wallet_bridge_api  # noqa: F401
+from schemas.apis.account_by_key_api import response_schemas as account_by_key_api
+from schemas.apis.account_history_api import response_schemas as account_history_api
+from schemas.apis.block_api import response_schemas as block_api
+from schemas.apis.condenser_api import response_schemas as condenser_api
+from schemas.apis.database_api import response_schemas as database_api
+from schemas.apis.debug_node_api import response_schemas as debug_node_api
+from schemas.apis.jsonrpc import response_schemas as jsonrpc
+from schemas.apis.market_history_api import response_schemas as market_history_api
+from schemas.apis.network_broadcast_api import response_schemas as network_broadcast_api
+from schemas.apis.network_node_api import response_schemas as network_node_api
+from schemas.apis.rc_api import response_schemas as rc_api
+from schemas.apis.reputation_api import response_schemas as reputation_api
+from schemas.apis.transaction_status_api import response_schemas as transaction_status_api
+from schemas.apis.wallet_bridge_api import response_schemas as wallet_bridge_api
 
-__locals = locals().copy()  # this is required, because creation of loop changes size of dict
-apis = {key: value for key, value in __locals.items() if key.endswith("_api") or key in ["jsonrpc"]}
+APIS: Final[dict[str, ModuleType]] = {
+    "account_by_key_api": account_by_key_api,
+    "account_history_api": account_history_api,
+    "block_api": block_api,
+    "condenser_api": condenser_api,
+    "database_api": database_api,
+    "debug_node_api": debug_node_api,
+    "jsonrpc": jsonrpc,
+    "market_history_api": market_history_api,
+    "network_broadcast_api": network_broadcast_api,
+    "network_node_api": network_node_api,
+    "rc_api": rc_api,
+    "reputation_api": reputation_api,
+    "transaction_status_api": transaction_status_api,
+    "wallet_bridge_api": wallet_bridge_api,
+}
 
 
 def get_schema(schema_name: str) -> type[PreconfiguredBaseModel]:
@@ -30,8 +46,8 @@ def get_schema(schema_name: str) -> type[PreconfiguredBaseModel]:
         return "".join([segment.title() for segment in text.split("_")])
 
     api, method = schema_name.split(".")
-    assert api in apis, f"Api `{api}` not found, currently supported are: {apis}"
-    schemas_module = apis[api]
+    assert api in APIS, f"Api `{api}` not found, currently supported are: {APIS}"
+    schemas_module = APIS[api]
 
     method = __snake_case_to_pascal_case(method)
     try:
