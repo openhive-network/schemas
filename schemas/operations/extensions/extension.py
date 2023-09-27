0aa2a0c4e92450dc1339e17c4722ab5b11b2d9d8
Mateusz Żebrak <mzebrak@syncad.com>
2023-09-27 10:11:05 +0200

    Create OperationExtension base class

---
+++ schemas/operations/extensions/extension.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from abc import ABC
+
+from schemas._preconfigured_base_model import PreconfiguredBaseModel
+
+
+class OperationExtension(PreconfiguredBaseModel, ABC):
+    __extension_name__: str
+
+    @classmethod
+    def get_name(cls) -> str:
+        return cls.__extension_name__
