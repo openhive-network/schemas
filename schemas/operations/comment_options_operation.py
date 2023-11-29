33bebbf2b811937a2f9b5b7a62cc91d853c59519
Radek Masłowski <rmaslowski@syncad.com>
2023-11-29 10:19:05 +0100

    Apply missing default values in comment_options_operation

--- schemas/operations/comment_options_operation.py
+++ schemas/operations/comment_options_operation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Any, Final, Generic
 
+from pydantic import Field
 from pydantic.generics import GenericModel
 
 from schemas.fields.assets.hbd import AssetHbdHF26, AssetHbdLegacy, AssetHbdT
@@ -12,6 +13,7 @@ from schemas.operation import Operation
 
 DEFAULT_ALLOW_VOTES: Final[bool] = True
 DEFAULT_ALLOW_CURATION_REWARDS: Final[bool] = True
+DEFAULT_MAX_ACCEPTED_PAYOUT: Final[AssetHbdHF26] = AssetHbdHF26(amount=1000000000)
 
 
 class _CommentOptionsOperation(Operation, GenericModel, Generic[AssetHbdT]):
@@ -24,12 +26,12 @@ class _CommentOptionsOperation(Operation, GenericModel, Generic[AssetHbdT]):
     percent_hbd: Uint16t = Uint16t(HIVE_100_PERCENT)
     allow_votes: bool = DEFAULT_ALLOW_VOTES
     allow_curation_rewards: bool = DEFAULT_ALLOW_CURATION_REWARDS
-    extensions: list[Any]
+    extensions: list[Any] = Field(default_factory=list)
 
 
 class CommentOptionsOperation(_CommentOptionsOperation[AssetHbdHF26]):
-    ...
+    max_accepted_payout: AssetHbdHF26 = DEFAULT_MAX_ACCEPTED_PAYOUT
 
 
 class CommentOptionsOperationLegacy(_CommentOptionsOperation[AssetHbdLegacy]):
-    ...
+    max_accepted_payout: AssetHbdLegacy = AssetHbdLegacy(DEFAULT_MAX_ACCEPTED_PAYOUT.as_legacy())
