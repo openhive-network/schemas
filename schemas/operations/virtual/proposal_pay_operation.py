972a7560a33f5ca4d5cc5e39645eb0626051e7e0
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-12-04 13:43:57 +0100

    Remove trx_id and op_in_trx from proposal pay operation

--- schemas/operations/virtual/proposal_pay_operation.py
+++ schemas/operations/virtual/proposal_pay_operation.py
@@ -8,12 +8,10 @@ from schemas.fields.assets.hbd import AssetHbdHF26, AssetHbdLegacy, AssetHbdT
 from schemas.fields.basic import (
     AccountName,
 )
-from schemas.fields.hex import TransactionId
-from schemas.fields.integers import Uint16t, Uint32t
+from schemas.fields.integers import Uint32t
 from schemas.virtual_operation import VirtualOperation
 
 DEFAULT_PROPOSAL_ID: Final[Uint32t] = Uint32t(0)
-DEFAULT_OP_IN_TRX: Final[Uint16t] = Uint16t(0)
 
 
 class _ProposalPayOperation(VirtualOperation, GenericModel, Generic[AssetHbdT]):
@@ -24,8 +22,6 @@ class _ProposalPayOperation(VirtualOperation, GenericModel, Generic[AssetHbdT]):
     receiver: AccountName
     payer: AccountName
     payment: AssetHbdT
-    trx_id: TransactionId
-    op_in_trx: Uint16t = DEFAULT_OP_IN_TRX
 
 
 class ProposalPayOperation(_ProposalPayOperation[AssetHbdHF26]):
