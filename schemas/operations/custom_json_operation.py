6098a4e522442223320f55247bbe32c2757bdd54
Jakub Ziębiński <ziebinskijakub@gmail.com>
2024-01-19 13:07:15 +0100

    Create a JsonString type so that no json to string conversions are made

--- schemas/operations/custom_json_operation.py
+++ schemas/operations/custom_json_operation.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-from typing import Any
+from pydantic import Field
 
-from pydantic import Field, Json
-
-from schemas.fields.basic import AccountName, CustomIdType, EmptyString
+from schemas.fields.basic import AccountName, CustomIdType
+from schemas.fields.json_string import JsonString
 from schemas.operation import Operation
 
 
@@ -15,4 +14,4 @@ class CustomJsonOperation(Operation):
     required_auths: list[AccountName]
     required_posting_auths: list[AccountName]
     id_: CustomIdType = Field(alias="id")
-    json_: Json[Any] | EmptyString = Field(alias="json")
+    json_: JsonString = Field(alias="json")
