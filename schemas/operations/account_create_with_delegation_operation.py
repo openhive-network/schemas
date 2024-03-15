7038e0fc870aacc8a28e8f6170b61a1da8b7e91c
Radek Masłowski <rmaslowski@syncad.com>
2024-03-15 14:39:49 +0100

    Create account_create_with_delegation_operation

---
+++ schemas/operations/account_create_with_delegation_operation.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Generic
+
+from pydantic.generics import GenericModel
+
+from schemas.fields.assets.hive import AssetHiveHF26, AssetHiveLegacy, AssetHiveT
+from schemas.fields.assets.vests import AssetVestsHF26, AssetVestsLegacy, AssetVestsT
+from schemas.fields.basic import (
+    AccountName,
+    PublicKey,
+)
+from schemas.fields.compound import Authority
+from schemas.operation import Operation
+
+
+class _AccountCreateWithDelegationOperation(Operation, GenericModel, Generic[AssetHiveT, AssetVestsT]):
+    __operation_name__ = "account_create_with_delegation"
+    __offset__ = 41
+
+    fee: AssetHiveT
+    delegation: AssetVestsT
+    creator: AccountName
+    new_account_name: AccountName
+    owner: Authority
+    active: Authority
+    posting: Authority
+    memo_key: PublicKey
+    json_metadata: str
+
+
+class AccountCreateWithDelegationOperation(_AccountCreateWithDelegationOperation[AssetHiveHF26, AssetVestsHF26]):
+    ...
+
+
+class AccountCreateWithDelegationOperationLegacy(
+    _AccountCreateWithDelegationOperation[AssetHiveLegacy, AssetVestsLegacy]
+):
+    ...
