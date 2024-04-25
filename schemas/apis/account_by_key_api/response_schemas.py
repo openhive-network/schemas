31029b332fbc49b17a90c8b694a3de78a7705935
kmochocki <kmochocki@syncad.com>
2024-04-25 05:25:20 +0000

    Fix GetKeyReferences; See: hive#682

--- schemas/apis/account_by_key_api/response_schemas.py
+++ schemas/apis/account_by_key_api/response_schemas.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 from schemas._preconfigured_base_model import PreconfiguredBaseModel
-from schemas.fields.basic import AccountName
+from schemas.fields.basic import AccountName, EmptyList
 
 
 class GetKeyReferences(PreconfiguredBaseModel):
-    accounts: list[tuple[AccountName]]
+    accounts: list[tuple[AccountName]] | list[EmptyList]
