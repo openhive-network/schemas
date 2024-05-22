c259064b2d4ffb629f8c8b45697e10078f84752e
Radek Masłowski <rmaslowski@syncad.com>
2024-05-22 15:29:56 +0200

    Implement follow and delegate_rc operations schemas

---
+++ schemas/operations/follow_operation.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from schemas.fields.basic import AccountName
+from schemas.operations.custom.custom_base_operation import CustomBaseOperation
+
+
+class FollowOperation(CustomBaseOperation):
+    __operation_name__ = "follow"
+
+    follower: AccountName
+    following: AccountName
+    what: list[str]
