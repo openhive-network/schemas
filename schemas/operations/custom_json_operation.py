1e51304ae74cd863f094aad6cb40fc8828d296d3
Marcin Sobczyk <msobczyk@syncad.com>
2024-06-05 18:18:26 +0200

    Use plain str type for json in CustomJsonOperation

--- schemas/operations/custom_json_operation.py
+++ schemas/operations/custom_json_operation.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from typing import Any
+from pydantic import Field
 
-from pydantic import Field, Json
-
-from schemas.fields.basic import AccountName, CustomIdType, EmptyString
+from schemas.fields.basic import AccountName, CustomIdType
 from schemas.operation import Operation
 
 
@@ -15,4 +13,4 @@ class CustomJsonOperation(Operation):
     required_auths: list[AccountName]
     required_posting_auths: list[AccountName]
     id_: CustomIdType = Field(alias="id")
-    json_: Json[Any] | EmptyString = Field(alias="json")
+    json_: str = Field(alias="json")
