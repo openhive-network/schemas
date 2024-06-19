aeef0e581b10bfecb0382bdf580b4ede88e3709b
Marcin Sobczyk <msobczyk@syncad.com>
2024-06-19 13:51:32 +0200

    Make JsonString field generic class

--- schemas/fields/json_string.py
+++ schemas/fields/json_string.py
@@ -1,37 +1,33 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING, Any, TypeAlias
-
-from pydantic import BaseModel
+from typing import TYPE_CHECKING, Any, Generic, TypeAlias, TypeVar
 
 if TYPE_CHECKING:
     from pydantic.typing import CallableGenerator
 
 
 AnyJson: TypeAlias = dict[str, "AnyJson"] | list["AnyJson"] | tuple["AnyJson", ...] | str | int | float | bool | None
+T = TypeVar("T")
 
 
-class JsonString:
+class JsonString(Generic[T]):
     """
     It must be possible to get and set json content, load JsonString value form string and dump JsonString to string.
     JsonString has property value which allows to get and set json content as dict, list, str, int, float, bool or None.
     """
 
-    def __init__(self, value: AnyJson) -> None:
-        self._value = value
+    def __init__(self, value: T) -> None:
+        self._value: T = value
 
     @classmethod
     def __get_validators__(cls) -> CallableGenerator:
         yield cls.validate
 
     @classmethod
-    def validate(cls, json_: AnyJson | str | BaseModel | JsonString) -> JsonString:
+    def validate(cls, json_: T | str | JsonString[T]) -> JsonString[T]:
         if isinstance(json_, JsonString):
             return json_
-
-        if isinstance(json_, BaseModel):
-            return cls(json_.dict())
 
         if isinstance(json_, str):
             try:
@@ -42,18 +38,18 @@ class JsonString:
         return cls(json_)
 
     @property
-    def value(self) -> AnyJson:
+    def value(self) -> T:
         return self._value
 
     @value.setter
-    def value(self, new_value: AnyJson) -> None:
+    def value(self, new_value: T) -> None:
         self._value = new_value
 
     def serialize(self) -> str:
         """Dumps JsonString with no spaces between keys and values"""
         return json.dumps(self._value, separators=(",", ":"))
 
-    def __getitem__(self, key: str) -> AnyJson:
+    def __getitem__(self, key: str) -> Any:
         if not isinstance(self._value, dict):
             raise TypeError(f"The value in JsonString must be dict to use subscript, got: `{type(self._value)}`")
         return self._value[key]
--- schemas/operations/custom_json_operation.py
+++ schemas/operations/custom_json_operation.py
@@ -1,17 +1,25 @@
 from __future__ import annotations
 
+from typing import Generic, TypeAlias, TypeVar
+
 from pydantic import Field
+from pydantic.generics import GenericModel
 
 from schemas.fields.basic import AccountName, CustomIdType
-from schemas.fields.json_string import JsonString
+from schemas.fields.json_string import AnyJson, JsonString
 from schemas.operation import Operation
 
+JsonFieldType = TypeVar("JsonFieldType")
 
-class CustomJsonOperation(Operation):
+
+class CustomJsonOperationGeneric(Operation, GenericModel, Generic[JsonFieldType]):
     __operation_name__ = "custom_json"
     __offset__ = 18
 
     required_auths: list[AccountName]
     required_posting_auths: list[AccountName]
     id_: CustomIdType = Field(alias="id")
-    json_: JsonString = Field(alias="json")
+    json_: JsonString[JsonFieldType] = Field(alias="json")
+
+
+CustomJsonOperation: TypeAlias = CustomJsonOperationGeneric[AnyJson]
