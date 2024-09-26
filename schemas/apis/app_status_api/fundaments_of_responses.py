7224dfb759a46d6225d6a5f98f0e34e156e93d1c
kmochocki <kmochocki@syncad.com>
2024-09-26 23:07:28 +0000

    Add app_status_api

---
+++ schemas/apis/app_status_api/fundaments_of_responses.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from pydantic import Field
+
+from schemas._preconfigured_base_model import PreconfiguredBaseModel
+from schemas.apis.app_status_api.block_stats import BlockStats
+from schemas.apis.app_status_api.rc_stats import FullRcStats, RegularRcStats
+from schemas.fields.hex import TransactionId
+from schemas.fields.hive_datetime import HiveDateTime
+
+KnownStatuses = Literal[
+    "signals attached",
+    "interrupted",
+    "syncing",
+    "syncing",
+    "entering live mode",
+    "finished syncing",
+    "replaying",
+    "finished replaying",
+    "exiting with open database error",
+    "finished syncing",
+    "P2P stopped",
+    "P2P started",
+    "loading snapshot",
+    "finished loading snapshot",
+    "dumping snapshot",
+    "finished dumping snapshot",
+    "beekeeper is starting",
+    "beekeeper is ready",
+    "opening beekeeper failed. Beekeeper API is disabled",
+    "chain API ready",
+]
+
+
+class BaseStatusItem(PreconfiguredBaseModel):
+    timestamp: HiveDateTime
+
+
+class StatusItem(BaseStatusItem):
+    status: KnownStatuses | str
+
+
+class WebserverItem(BaseStatusItem):
+    address: str
+    port: int
+
+
+class SwitchingForkItem(PreconfiguredBaseModel):
+    id_: TransactionId = Field(alias="id")
+    num: int
+
+
+class HivedBenchmark(PreconfiguredBaseModel):
+    n: int
+    """current block number"""
+    rt: int
+    """measured real time (in microseconds)"""
+    ct: int
+    """measured cpu time (in microseconds)"""
+    cm: int
+    """measured consumed memory"""
+    pm: int
+    """measured peak in consumed memory"""
+
+
+class RcStatsItem(PreconfiguredBaseModel):
+    rc_stats: RegularRcStats | FullRcStats
+
+
+class BlockStatsItem(PreconfiguredBaseModel):
+    block_stats: BlockStats
+
+
+class HivedBenchmarkItem(PreconfiguredBaseModel):
+    multiindex_stats: HivedBenchmark
+
+
+class KnownRecords(PreconfiguredBaseModel):
+    rc_stats: RcStatsItem | None = None
+    switching_forks: SwitchingForkItem | None = None
+    hived_benchmark: HivedBenchmarkItem | None = None
+    block_stats: BlockStatsItem | None = None
+
+
+class KnownWebservers(PreconfiguredBaseModel):
+    WS: WebserverItem | None = None
+    HTTP: WebserverItem | None = None
+    P2P: WebserverItem | None = None
