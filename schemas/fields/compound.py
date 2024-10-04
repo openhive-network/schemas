a2ffb69a28fab9e1404353a52269eb47e2187326
Mateusz Żebrak <mzebrak@syncad.com>
2024-10-04 11:22:25 +0200

    Fix typo sbd -> hbd

--- schemas/fields/compound.py
+++ schemas/fields/compound.py
@@ -149,11 +149,9 @@ WitnessPropsSerializedKey = Literal[
     "key",
     "maximum_block_size",
     "new_signing_key",
-    "sbd_exchange_rate",
-    "sbd_interest_rate",
-    "url",
     "hbd_exchange_rate",
     "hbd_interest_rate",
+    "url",
 ]
 
 WitnessPropsSerialized = list[tuple[WitnessPropsSerializedKey, Hex]]
