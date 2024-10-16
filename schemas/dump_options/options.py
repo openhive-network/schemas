ca7e244c396eea4ecffd1d8eadfc79bb489be99b
“Mariusz Trela” <mtrela@syncad.com>
2024-10-16 12:07:09 +0200

    Add schema for `config/command_line` options

---
+++ schemas/dump_options/options.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from schemas._preconfigured_base_model import PreconfiguredBaseModel
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+__all__ = [
+    "OptionValue",
+    "Option",
+    "Options",
+]
+
+
+class OptionValue(PreconfiguredBaseModel):
+    required: bool
+    multitoken: bool
+    composed: bool
+    value_type: Path | list[str] | str | bool | int
+    default_value: Path | list[str] | str | bool | int
+
+
+class Option(PreconfiguredBaseModel):
+    name: str
+    description: str
+    value: OptionValue | None
+
+
+class Options(PreconfiguredBaseModel):
+    common: list[Option] | None
+    config_file: list[Option] | None
+    command_line: list[Option] | None
