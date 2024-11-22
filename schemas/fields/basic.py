e537898a180794a082a962ac3367f17768f517c7
Radek Masłowski <rmaslowski@syncad.com>
2024-11-22 11:19:02 +0100

    step 1

--- schemas/fields/basic.py
+++ schemas/fields/basic.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import re
-from typing import TYPE_CHECKING, Any, Final
+from typing import TYPE_CHECKING, Annotated, Any, Final
 
-from pydantic import StringConstraints, errors
+from pydantic import Field, StringConstraints
 
 __all__ = [
     "AccountName",
@@ -18,12 +18,9 @@ __all__ = [
     "Url",
 ]
 
-from pydantic.validators import list_validator
 
 from schemas.hive_constants import HIVE_MAX_URL_LENGTH, HIVE_MAX_WITNESS_URL_LENGTH
 
-if TYPE_CHECKING:
-    from pydantic.typing import CallableGenerator
 
 ACCOUNT_NAME_SEGMENT_REGEX: Final[str] = r"[a-z][a-z0-9\-]+[a-z0-9]"
 BASE_58_REGEX: Final[str] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
@@ -43,21 +40,7 @@ class EmptyString(StringConstraints):
     min_length = 0
     max_length = 0
 
-
-class EmptyList(list):  # type: ignore[type-arg] # See pydantic.ConstrainedList
-    @classmethod
-    # TODO[pydantic]: We couldn't refactor `__get_validators__`, please create the `__get_pydantic_core_schema__` manually.
-    # Check https://docs.pydantic.dev/latest/migration/#defining-custom-types for more information.
-    def __get_validators__(cls) -> CallableGenerator:
-        yield cls.list_length_validator
-
-    @classmethod
-    def list_length_validator(cls, v: Any) -> list[Any]:
-        v = list_validator(v)
-        if len(v) > 0:
-            raise errors.ListMaxLengthError(limit_value=0)
-        return []
-
+EmptyList = Annotated[list[Any], Field(max_length=0)]
 
 class FloatAsString(StringConstraints):
     regex = re.compile(r"^(?:(?:[1-9][0-9]*)|0)\.[0-9]+$")
