95228a985dde2e5fe9b4f4f4587136a78768a1c2
Radek Masłowski <rmaslowski@syncad.com>
2025-02-17 10:24:55 +0100

    fixup! Refactor operations schemas

--- schemas/operations/virtual/__init__.py
+++ schemas/operations/virtual/__init__.py
@@ -1,5 +1,47 @@
 from __future__ import annotations
 
+from schemas.operations.virtual.account_created_operation import AccountCreatedOperation
+from schemas.operations.virtual.author_reward_operation import AuthorRewardOperation
+from schemas.operations.virtual.changed_recovery_account_operation import ChangedRecoveryAccountOperation
+from schemas.operations.virtual.clear_null_account_balance_operation import ClearNullAccountBalanceOperation
+from schemas.operations.virtual.collateralized_convert_immediate_conversion_operation import (
+    CollateralizedConvertImmediateConversionOperation,
+)
+from schemas.operations.virtual.comment_benefactor_reward_operation import CommentBenefactorRewardOperation
+from schemas.operations.virtual.comment_payout_update_operation import CommentPayoutUpdateOperation
+from schemas.operations.virtual.comment_reward_operation import CommentRewardOperation
+from schemas.operations.virtual.consolidate_treasury_balance_operation import ConsolidateTreasuryBalanceOperation
+from schemas.operations.virtual.curation_reward_operation import CurationRewardOperation
+from schemas.operations.virtual.declined_voting_rights_operation import DeclinedVotingRightsOperation
+from schemas.operations.virtual.delayed_voting_operation import DelayedVotingOperation
+from schemas.operations.virtual.dhf_conversion_operation import DhfConversionOperation
+from schemas.operations.virtual.dhf_funding_operation import DhfFundingOperation
+from schemas.operations.virtual.effective_comment_vote_operation import EffectiveCommentVoteOperation
+from schemas.operations.virtual.escrow_approved_operation import EscrowApprovedOperation
+from schemas.operations.virtual.escrow_rejected_operation import EscrowRejectedOperation
+from schemas.operations.virtual.expired_account_notification_operation import ExpiredAccountNotificationOperation
+from schemas.operations.virtual.failed_recurrent_transfer_operation import FailedRecurrentTransferOperation
+from schemas.operations.virtual.fill_collateralized_convert_request_operation import (
+    FillCollateralizedConvertRequestOperation,
+)
+from schemas.operations.virtual.fill_convert_request_operation import FillConvertRequestOperation
+from schemas.operations.virtual.fill_order_operation import FillOrderOperation
+from schemas.operations.virtual.fill_recurrent_transfer_operation import FillRecurrentTransferOperation
+from schemas.operations.virtual.fill_transfer_from_savings_operation import FillTransferFromSavingsOperation
+from schemas.operations.virtual.fill_vesting_withdraw_operation import FillVestingWithdrawOperation
+from schemas.operations.virtual.hardfork_hive_operation import HardforkHiveOperation
+from schemas.operations.virtual.hardfork_hive_restore_operation import HardforkHiveRestoreOperation
+from schemas.operations.virtual.hardfork_operation import HardforkOperation
+from schemas.operations.virtual.ineffective_delete_comment_operation import IneffectiveDeleteCommentOperation
+from schemas.operations.virtual.interest_operation import InterestOperation
+from schemas.operations.virtual.limit_order_cancelled_operation import LimitOrderCancelledOperation
+from schemas.operations.virtual.liquidity_reward_operation import LiquidityRewardOperation
+from schemas.operations.virtual.pow_reward_operation import PowRewardOperation
+from schemas.operations.virtual.producer_missed_operation import ProducerMissedOperation
+from schemas.operations.virtual.producer_reward_operation import ProducerRewardOperation
+from schemas.operations.virtual.proposal_fee_operation import ProposalFeeOperation
+from schemas.operations.virtual.proposal_pay_operation import ProposalPayOperation
+from schemas.operations.virtual.proxy_cleared_operation import ProxyClearedOperation
 from schemas.operations.virtual.representation_types import (
     HF26RepresentationAccountCreatedOperation,
     HF26RepresentationAuthorRewardOperation,
@@ -88,6 +130,11 @@ from schemas.operations.virtual.representation_types import (
     LegacyRepresentationTransferToVestingCompletedOperation,
     LegacyRepresentationVestingSharesSplitOperation,
 )
+from schemas.operations.virtual.return_vesting_delegation_operation import ReturnVestingDelegationOperation
+from schemas.operations.virtual.shutdown_witness_operation import ShutDownWitnessOperation
+from schemas.operations.virtual.system_warning_operation import SystemWarningOperation
+from schemas.operations.virtual.transfer_to_vesting_completed_operation import TransferToVestingCompletedOperation
+from schemas.operations.virtual.vesting_shares_split_operation import VestingSharesSplitOperation
 
 __all__ = [
     # ANY VIRTUAL OPERATION
@@ -263,3 +310,97 @@ LegacyVirtualOperationRepresentation = (
     | LegacyRepresentationTransferToVestingCompletedOperation
     | LegacyRepresentationVestingSharesSplitOperation
 )
+
+
+HF26RepresentationAndValuePairsVirtual = {
+    AuthorRewardOperation: HF26RepresentationAuthorRewardOperation,
+    AccountCreatedOperation: HF26RepresentationAccountCreatedOperation,
+    ChangedRecoveryAccountOperation: HF26RepresentationChangedRecoveryAccountOperation,
+    ClearNullAccountBalanceOperation: HF26RepresentationClearNullAccountBalanceOperation,
+    CollateralizedConvertImmediateConversionOperation: HF26RepresentationCollateralizedConvertImmediateConversionOperation,
+    CommentBenefactorRewardOperation: HF26RepresentationCommentBenefactorRewardOperation,
+    CommentPayoutUpdateOperation: HF26RepresentationCommentPayoutUpdateOperation,
+    CommentRewardOperation: HF26RepresentationCommentRewardOperation,
+    ConsolidateTreasuryBalanceOperation: HF26RepresentationConsolidateTreasuryBalanceOperation,
+    CurationRewardOperation: HF26RepresentationCurationRewardOperation,
+    DeclinedVotingRightsOperation: HF26RepresentationDeclinedVotingRightsOperation,
+    DelayedVotingOperation: HF26RepresentationDelayedVotingOperation,
+    DhfConversionOperation: HF26RepresentationDhfConversionOperation,
+    DhfFundingOperation: HF26RepresentationDhfFundingOperation,
+    EffectiveCommentVoteOperation: HF26RepresentationEffectiveCommentVoteOperation,
+    EscrowApprovedOperation: HF26RepresentationEscrowApprovedOperation,
+    EscrowRejectedOperation: HF26RepresentationEscrowRejectedOperation,
+    ExpiredAccountNotificationOperation: HF26RepresentationExpiredAccountNotificationOperation,
+    FailedRecurrentTransferOperation: HF26RepresentationFailedRecurrentTransferOperation,
+    FillCollateralizedConvertRequestOperation: HF26RepresentationFillCollateralizedConvertRequestOperation,
+    FillConvertRequestOperation: HF26RepresentationFillConvertRequestOperation,
+    FillOrderOperation: HF26RepresentationFillOrderOperation,
+    FillRecurrentTransferOperation: HF26RepresentationFillRecurrentTransferOperation,
+    FillTransferFromSavingsOperation: HF26RepresentationFillTransferFromSavingsOperation,
+    FillVestingWithdrawOperation: HF26RepresentationFillVestingWithdrawOperation,
+    HardforkHiveOperation: HF26RepresentationHardforkHiveOperation,
+    HardforkHiveRestoreOperation: HF26RepresentationHardforkHiveRestoreOperation,
+    HardforkOperation: HF26RepresentationHardforkOperation,
+    IneffectiveDeleteCommentOperation: HF26RepresentationIneffectiveDeleteCommentOperation,
+    InterestOperation: HF26RepresentationInterestOperation,
+    LimitOrderCancelledOperation: HF26RepresentationLimitOrderCancelledOperation,
+    LiquidityRewardOperation: HF26RepresentationLiquidityRewardOperation,
+    PowRewardOperation: HF26RepresentationPowRewardOperation,
+    ProducerMissedOperation: HF26RepresentationProducerMissedOperation,
+    ProducerRewardOperation: HF26RepresentationProducerRewardOperation,
+    ProposalFeeOperation: HF26RepresentationProposalFeeOperation,
+    ProposalPayOperation: HF26RepresentationProposalPayOperation,
+    ProxyClearedOperation: HF26RepresentationProxyClearedOperation,
+    ReturnVestingDelegationOperation: HF26RepresentationReturnVestingDelegationOperation,
+    ShutDownWitnessOperation: HF26RepresentationShutDownWitnessOperation,
+    SystemWarningOperation: HF26RepresentationSystemWarningOperation,
+    TransferToVestingCompletedOperation: HF26RepresentationTransferToVestingCompletedOperation,
+    VestingSharesSplitOperation: HF26RepresentationVestingSharesSplitOperation,
+}
+
+
+LegacyRepresentationAndValuePairsVirtual = {
+    AuthorRewardOperation: LegacyRepresentationAuthorRewardOperation,
+    AccountCreatedOperation: LegacyRepresentationAccountCreatedOperation,
+    ChangedRecoveryAccountOperation: LegacyRepresentationChangedRecoveryAccountOperation,
+    ClearNullAccountBalanceOperation: LegacyRepresentationClearNullAccountBalanceOperation,
+    CollateralizedConvertImmediateConversionOperation: LegacyRepresentationCollateralizedConvertImmediateConversionOperation,
+    CommentBenefactorRewardOperation: LegacyRepresentationCommentBenefactorRewardOperation,
+    CommentPayoutUpdateOperation: LegacyRepresentationCommentPayoutUpdateOperation,
+    CommentRewardOperation: LegacyRepresentationCommentRewardOperation,
+    ConsolidateTreasuryBalanceOperation: LegacyRepresentationConsolidateTreasuryBalanceOperation,
+    CurationRewardOperation: LegacyRepresentationCurationRewardOperation,
+    DeclinedVotingRightsOperation: LegacyRepresentationDeclinedVotingRightsOperation,
+    DelayedVotingOperation: LegacyRepresentationDelayedVotingOperation,
+    DhfConversionOperation: LegacyRepresentationDhfConversionOperation,
+    DhfFundingOperation: LegacyRepresentationDhfFundingOperation,
+    EffectiveCommentVoteOperation: LegacyRepresentationEffectiveCommentVoteOperation,
+    EscrowApprovedOperation: LegacyRepresentationEscrowApprovedOperation,
+    EscrowRejectedOperation: LegacyRepresentationEscrowRejectedOperation,
+    ExpiredAccountNotificationOperation: LegacyRepresentationExpiredAccountNotificationOperation,
+    FailedRecurrentTransferOperation: LegacyRepresentationFailedRecurrentTransferOperation,
+    FillCollateralizedConvertRequestOperation: LegacyRepresentationFillCollateralizedConvertRequestOperation,
+    FillConvertRequestOperation: LegacyRepresentationFillConvertRequestOperation,
+    FillOrderOperation: LegacyRepresentationFillOrderOperation,
+    FillRecurrentTransferOperation: LegacyRepresentationFillRecurrentTransferOperation,
+    FillTransferFromSavingsOperation: LegacyRepresentationFillTransferFromSavingsOperation,
+    FillVestingWithdrawOperation: LegacyRepresentationFillVestingWithdrawOperation,
+    HardforkHiveOperation: LegacyRepresentationHardforkHiveOperation,
+    HardforkHiveRestoreOperation: LegacyRepresentationHardforkHiveRestoreOperation,
+    HardforkOperation: LegacyRepresentationHardforkOperation,
+    IneffectiveDeleteCommentOperation: LegacyRepresentationIneffectiveDeleteCommentOperation,
+    InterestOperation: LegacyRepresentationInterestOperation,
+    LimitOrderCancelledOperation: LegacyRepresentationLimitOrderCancelledOperation,
+    LiquidityRewardOperation: LegacyRepresentationLiquidityRewardOperation,
+    PowRewardOperation: LegacyRepresentationPowRewardOperation,
+    ProducerMissedOperation: LegacyRepresentationProducerMissedOperation,
+    ProducerRewardOperation: LegacyRepresentationProducerRewardOperation,
+    ProposalFeeOperation: LegacyRepresentationProposalFeeOperation,
+    ProposalPayOperation: LegacyRepresentationProposalPayOperation,
+    ProxyClearedOperation: LegacyRepresentationProxyClearedOperation,
+    ReturnVestingDelegationOperation: LegacyRepresentationReturnVestingDelegationOperation,
+    ShutDownWitnessOperation: LegacyRepresentationShutDownWitnessOperation,
+    SystemWarningOperation: LegacyRepresentationSystemWarningOperation,
+    TransferToVestingCompletedOperation: LegacyRepresentationTransferToVestingCompletedOperation,
+    VestingSharesSplitOperation: LegacyRepresentationVestingSharesSplitOperation,
+}
