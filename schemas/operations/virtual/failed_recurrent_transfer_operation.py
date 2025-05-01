3cd8a4e1a2fded4733fd39753385fd54b995335f
Martin Lees <mlees@oxio.io>
2025-05-01 17:25:04 +0900

    added pair_id to recurrent transfers

--- schemas/operations/virtual/failed_recurrent_transfer_operation.py
+++ schemas/operations/virtual/failed_recurrent_transfer_operation.py
@@ -16,6 +16,7 @@ from schemas.virtual_operation import VirtualOperation
 DEFAULT_CONSECUTIVE_FAILURES: Final[Uint8t] = Uint8t(0)
 DEFAULT_REMAINING_EXECUTIONS: Final[Uint16t] = Uint16t(0)
 DEFAULT_DELETED: Final[bool] = False
+DEFAULT_PAIR_ID: Final[Uint8t] = Uint8t(0)
 
 
 class _FailedRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[AssetHiveT, AssetHbdT]):
@@ -29,6 +30,7 @@ class _FailedRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[
     consecutive_failures: Uint8t = DEFAULT_CONSECUTIVE_FAILURES
     remaining_executions: Uint16t = DEFAULT_REMAINING_EXECUTIONS
     deleted: bool = DEFAULT_DELETED
+    pair_id: Uint8t = DEFAULT_PAIR_ID
 
 
 class FailedRecurrentTransferOperation(_FailedRecurrentTransferOperation[AssetHiveHF26, AssetHbdHF26]):
--- schemas/operations/virtual/fill_recurrent_transfer_operation.py
+++ schemas/operations/virtual/fill_recurrent_transfer_operation.py
@@ -14,7 +14,7 @@ from schemas.fields.integers import Uint16t
 from schemas.virtual_operation import VirtualOperation
 
 DEFAULT_REMAINING_EXECUTIONS: Final[Uint16t] = Uint16t(0)
-
+DEFAULT_PAIR_ID: Final[Uint8t] = Uint8t(0)
 
 class _FillRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[AssetHiveT, AssetHbdT]):
     __operation_name__ = "fill_recurrent_transfer"
@@ -25,7 +25,7 @@ class _FillRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[As
     amount: AssetHiveT | AssetHbdT
     memo: str
     remaining_executions: Uint16t = DEFAULT_REMAINING_EXECUTIONS
-
+    pair_id: Uint8t = DEFAULT_PAIR_ID
 
 class FillRecurrentTransferOperation(_FillRecurrentTransferOperation[AssetHiveHF26, AssetHbdHF26]):
     ...
