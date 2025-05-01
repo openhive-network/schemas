29c69909b13a703234d72d60c9ae848c3327b843
Martin Lees <mlees@oxio.io>
2025-05-01 17:35:43 +0900

    black format

--- schemas/operations/virtual/fill_recurrent_transfer_operation.py
+++ schemas/operations/virtual/fill_recurrent_transfer_operation.py
@@ -16,6 +16,7 @@ from schemas.virtual_operation import VirtualOperation
 DEFAULT_REMAINING_EXECUTIONS: Final[Uint16t] = Uint16t(0)
 DEFAULT_PAIR_ID: Final[Uint8t] = Uint8t(0)
 
+
 class _FillRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[AssetHiveT, AssetHbdT]):
     __operation_name__ = "fill_recurrent_transfer"
     __offset__ = 33
@@ -27,9 +28,8 @@ class _FillRecurrentTransferOperation(VirtualOperation, GenericModel, Generic[As
     remaining_executions: Uint16t = DEFAULT_REMAINING_EXECUTIONS
     pair_id: Uint8t = DEFAULT_PAIR_ID
 
-class FillRecurrentTransferOperation(_FillRecurrentTransferOperation[AssetHiveHF26, AssetHbdHF26]):
-    ...
+
+class FillRecurrentTransferOperation(_FillRecurrentTransferOperation[AssetHiveHF26, AssetHbdHF26]): ...
 
 
-class FillRecurrentTransferOperationLegacy(_FillRecurrentTransferOperation[AssetHiveLegacy, AssetHbdLegacy]):
-    ...
+class FillRecurrentTransferOperationLegacy(_FillRecurrentTransferOperation[AssetHiveLegacy, AssetHbdLegacy]): ...
