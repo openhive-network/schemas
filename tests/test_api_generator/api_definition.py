f4719e5c867ba1ac3de06efb43ec7f0cd3d6f974
Jakub Ziebinski <jziebinski@gmail.com>
2025-05-20 09:47:06 +0000

    Add test with generation from the swagger json file

--- tests/test_api_generator/api_definition.py
+++ tests/test_api_generator/api_definition.py
@@ -16,7 +16,25 @@ class TestApiParams:
     some_int: int = 0
 
 
+@dataclass
+class SecondEndpointResponseItem:
+    some_string: str | None = None
+    some_bool: bool | None = None
+
+
 TestApiResult = str
+
+VALID_ENDPOINT_FROM_SWAGGER_PARAMS: dict[str, Any] = {
+    "return": str,
+    "some_string": str,
+    "some_integer": int,
+    "some_another_string": str,
+}
+VALID_ENDPOINT_FROM_SWAGGER_PARAMS_LIST_RETURN: dict[str, Any] = {
+    "some_string": str,
+    "some_integer": int,
+    "some_another_string": str,
+}
 
 VALID_ENDPOINT_PARAMS: dict[str, Any] = {
     "return": str,
