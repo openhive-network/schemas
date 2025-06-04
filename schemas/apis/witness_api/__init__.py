91ae9063552e065ec0492bd00ce1e85bbc9a0b9e
kmochocki <kmochocki@syncad.com>
2025-06-04 12:39:52 +0000

    Implement missing `witness-api` schema

---
+++ schemas/apis/witness_api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from schemas.apis.witness_api.response_schemas import (
+    DisableFastConfirm,
+    EnableFastConfirm,
+    IsFastConfirmEnabled,
+)
+
+__all__ = [
+    "EnableFastConfirm",
+    "DisableFastConfirm",
+    "IsFastConfirmEnabled",
+]
