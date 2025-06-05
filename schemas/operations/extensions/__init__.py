5130f2e5eae272cacfe1c19937b13d13e10dd4d2
Radek Masłowski <rmaslowski@syncad.com>
2025-06-05 06:28:15 +0000

    Replace representations dynamic functions with representations files and generators

---
+++ schemas/operations/extensions/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from schemas.operations.extensions.comment_options_extensions import CommentPayoutBeneficiaries
+from schemas.operations.extensions.recurrent_transfer_extensions import RecurrentTransferPairId
+from schemas.operations.extensions.update_proposal_extensions import UpdateProposalEndDate
+
+__all__ = [
+    "CommentPayoutBeneficiaries",
+    "RecurrentTransferPairId",
+    "UpdateProposalEndDate",
+]
