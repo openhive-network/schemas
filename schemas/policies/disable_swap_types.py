542c5f7618b39cff1b478806de80e846836267f2
Radek Masłowski <rmaslowski@syncad.com>
2025-07-09 09:11:46 +0000

    Add Policy sufix to policies

--- schemas/policies/disable_swap_types.py
+++ schemas/policies/disable_swap_types.py
@@ -7,13 +7,13 @@ from schemas.policies.policy import Policy
 
 
 @dataclass
-class DisableSwapTypes(Policy):
+class DisableSwapTypesPolicy(Policy):
     is_swap_types_disabled: ClassVar[bool] = False
     disabled: bool = False
 
     def apply(self) -> None:
-        DisableSwapTypes.is_swap_types_disabled = self.disabled
+        DisableSwapTypesPolicy.is_swap_types_disabled = self.disabled
 
     @classmethod
     def is_disabled(cls) -> bool:
-        return DisableSwapTypes.is_swap_types_disabled
+        return DisableSwapTypesPolicy.is_swap_types_disabled
