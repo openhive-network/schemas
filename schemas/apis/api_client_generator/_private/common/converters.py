ef7b29778cff13b04432a65cc07ec05836c63e8f
Jakub Ziebinski <jziebinski@gmail.com>
2025-07-16 09:33:25 +0000

    Implementation of the hyphen to snake converter

--- schemas/apis/api_client_generator/_private/common/converters.py
+++ schemas/apis/api_client_generator/_private/common/converters.py
@@ -12,3 +12,8 @@ def camel_to_snake(name: str) -> str:
     """Converts a CamelCase string to snake_case."""
     s1 = re.sub(r"(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+
+
+def hyphen_to_snake(name: str) -> str:
+    """Converts a hyphenated-string to snake_case."""
+    return name.replace("-", "_")
