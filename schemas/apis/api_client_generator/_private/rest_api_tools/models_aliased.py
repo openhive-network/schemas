cd3867a8f021fc8a04376892fc549b68a3d40a36
Jakub Ziebinski <jziebinski@gmail.com>
2025-07-17 12:52:42 +0000

    Add models aliased module for the rest api

---
+++ schemas/apis/api_client_generator/_private/rest_api_tools/models_aliased.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import ast
+from typing import Literal, TypeGuard, get_args
+
+PathParam = ast.arg
+QueryParam = ast.arg
+
+RestApiParameterType = Literal[
+    "query",
+    "path",
+]
+RestApiMethodType = Literal[
+    "get",
+    "post",
+    "put",
+    "patch",
+    "delete",
+]
+RestApiMethods: tuple[RestApiMethodType, ...] = get_args(RestApiMethodType)
+
+
+def is_valid_rest_api_method_type(value: str) -> TypeGuard[RestApiMethodType]:
+    """
+    Check if the given value is a valid REST API method type.
+
+    Args:
+        value: The value to check.
+
+    Returns:
+        True if the value is a valid REST API method type, False otherwise.
+    """
+    return value in RestApiMethods
