956cbd1d2f5391a26f822470dc913022d90d2963
Radek Masłowski <rmaslowski@syncad.com>
2025-08-04 08:18:20 +0000

    Removed "if typing hack"

--- schemas/fields/basic.py
+++ schemas/fields/basic.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Annotated, Final
+from typing import Annotated, Final
 
 import msgspec
 
@@ -29,39 +29,27 @@ ACCOUNT_NAME_REGEX = "^" + ACCOUNT_NAME_SEGMENT_REGEX + r"(:?\.{1}" + ACCOUNT_NA
 EmptyList = Annotated[list[int], msgspec.Meta(max_length=0)]
 EmptyString = Annotated[str, msgspec.Meta(max_length=0, min_length=0)]
 
-if TYPE_CHECKING:
-    AccountName = str
-    OptionallyEmptyAccountName = str
-    CustomIdType = str
-    FloatAsString = str
-    NodeType = str
-    Permlink = str
-    PublicKey = str
-    PrivateKey = str
-    WitnessUrl = str
-    Url = str
-else:
-    AccountName = ValidatorString.factory(
-        "AccountName",
-        msgspec.Meta(
-            max_length=16,
-            min_length=3,
-            pattern=ACCOUNT_NAME_REGEX,
-        ),
-    )
+AccountName = ValidatorString.factory(
+    "AccountName",
+    msgspec.Meta(
+        max_length=16,
+        min_length=3,
+        pattern=ACCOUNT_NAME_REGEX,
+    ),
+)
 
-    OptionallyEmptyAccountName = ValidatorString.factory(
-        "OptionallyEmptyAccountName",
-        msgspec.Meta(
-            max_length=16,
-            pattern=f"{ACCOUNT_NAME_REGEX}|^()$",
-        ),
-    )
-    CustomIdType = ValidatorString.factory("CustomIdType", msgspec.Meta(max_length=32))
-    FloatAsString = ValidatorString.factory("FloatAsString", msgspec.Meta(pattern=r"^(?:(?:[1-9][0-9]*)|0)\.[0-9]+$"))
-    NodeType = ValidatorString.factory("NodeType", msgspec.Meta(pattern=r"^(mainnet|testnet|mirrornet)$"))
-    Permlink = ValidatorString.factory("Permlink", msgspec.Meta(max_length=256))
-    PublicKey = ValidatorString.factory("PublicKey", msgspec.Meta(pattern=rf"^(?:STM)[{BASE_58_REGEX}]{{7,51}}$"))
-    PrivateKey = ValidatorString.factory("PrivateKey", msgspec.Meta(pattern=rf"^[{BASE_58_REGEX}]{{51}}$"))
-    WitnessUrl = ValidatorString.factory("WitnessUrl", msgspec.Meta(max_length=HIVE_MAX_WITNESS_URL_LENGTH))
-    Url = ValidatorString.factory("Url", msgspec.Meta(max_length=HIVE_MAX_URL_LENGTH))
+OptionallyEmptyAccountName = ValidatorString.factory(
+    "OptionallyEmptyAccountName",
+    msgspec.Meta(
+        max_length=16,
+        pattern=f"{ACCOUNT_NAME_REGEX}|^()$",
+    ),
+)
+CustomIdType = ValidatorString.factory("CustomIdType", msgspec.Meta(max_length=32))
+FloatAsString = ValidatorString.factory("FloatAsString", msgspec.Meta(pattern=r"^(?:(?:[1-9][0-9]*)|0)\.[0-9]+$"))
+NodeType = ValidatorString.factory("NodeType", msgspec.Meta(pattern=r"^(mainnet|testnet|mirrornet)$"))
+Permlink = ValidatorString.factory("Permlink", msgspec.Meta(max_length=256))
+PublicKey = ValidatorString.factory("PublicKey", msgspec.Meta(pattern=rf"^(?:STM)[{BASE_58_REGEX}]{{7,51}}$"))
+PrivateKey = ValidatorString.factory("PrivateKey", msgspec.Meta(pattern=rf"^[{BASE_58_REGEX}]{{51}}$"))
+WitnessUrl = ValidatorString.factory("WitnessUrl", msgspec.Meta(max_length=HIVE_MAX_WITNESS_URL_LENGTH))
+Url = ValidatorString.factory("Url", msgspec.Meta(max_length=HIVE_MAX_URL_LENGTH))
